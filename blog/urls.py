43ddd5b9db479c376981843e81f8409ef2f75773
Satish-baraduya <baradiyasatish@gmail.com>
2022-04-01 21:53:14 +0530

    views added

--- blog/urls.py
+++ blog/urls.py
@@ -12,6 +12,17 @@ urlpatterns = [
          views.PostDelete.as_view(), name='post_remove'),
     path(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
 
+    path(r'^post/(?P<pk>\d+)/comment/$',
+         views.add_comment_to_post, name='add_comment_to_post'),
+    path(r'^comment/(?P<pk>\d+)/approve/$',
+         views.comment_approve, name='comment_approve'),
+    path(r'^comment/(?P<pk>\d+)/remove/$',
+         views.commenT_remove, name='comment_remove'),
+
+    path(r'^post/(?P<pk>\d+)/publish/$',
+         views.post_publish, name='post_publish'),
+
+
 
 
 ]
