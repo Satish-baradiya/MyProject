1cc098f67e00e914a5d6c6d21864fa93c7125262
Satish-baraduya <baradiyasatish@gmail.com>
2022-04-01 19:19:11 +0530

    First Commit

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(r'^$', views.PostListView.as_view(), name='post_list'),
+    path(r'^about/$', views.AboutView.as_view(), name='about'),
+    path(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    path(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
+    path(r'^post/(?P<pk>\d+)/edit/$',
+         views.PostUpdateView.as_view(), name='about'),
+    path(r'^post/(?P<pk>\d+)/remove/$',
+         views.PostDelete.as_view(), name='post_remove'),
+    path(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
+
+
+
+]
